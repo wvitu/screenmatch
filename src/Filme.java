5d58b196e4340e396666375739dc934365fc3651
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-05-09 20:55:33 -0300

    Atributos de filme e avaliações adicionados
    Adicionei o método de avaliação e novos atributos do filme

--- src/Filme.java
+++ src/Filme.java
@@ -1,10 +1,25 @@
 public class Filme {
     String nome;
+    String diretor;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        System.out.println("Duração em Minutos: " + duracaoEmMinutos);
+        System.out.println("O filme é dirigido por: " + diretor);
+    }
 
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double retornaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,16 @@ public class Principal {
         meuFilme.nome = "Harry Potter e a pedra filosofal";
         meuFilme.anoDeLancamento = 2001;
         meuFilme.duracaoEmMinutos = 152;
+        meuFilme.diretor = "Chris Columbus";
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(9);
+        meuFilme.avalia(9.5);
+        meuFilme.avalia(9.75);
+        meuFilme.avalia(8.75);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.retornaMedia());
 
     }
 }
