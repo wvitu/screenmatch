e569fec954d024808785d9adcba3cf98cee66b76
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-05-09 20:31:12 -0300

    Inicio projeto plataforma de filme
    Atributos de filme definidos

---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "Harry Potter e a pedra filosofal";
+        meuFilme.anoDeLancamento = 2001;
+        meuFilme.duracaoEmMinutos = 152;
+
+    }
+}
