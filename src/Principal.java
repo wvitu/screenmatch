67d7f73925a38e1287ec70f685fe26418ac6d8e0
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-05-09 22:34:39 -0300

    Get e Set adicionados

--- src/Principal.java
+++ src/Principal.java
@@ -1,19 +1,21 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Harry Potter e a pedra filosofal";
-        meuFilme.anoDeLancamento = 2001;
-        meuFilme.duracaoEmMinutos = 152;
-        meuFilme.diretor = "Chris Columbus";
+        meuFilme.setNome("Harry Potter e a pedra filosofal");
+        meuFilme.setAnoDeLancamento(2001);
+        meuFilme.setDuracaoEmMinutos(152);
+        meuFilme.setDiretor("Chris Columbus");
+
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(9);
         meuFilme.avalia(9.5);
         meuFilme.avalia(9.75);
         meuFilme.avalia(8.75);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
-        System.out.println(meuFilme.retornaMedia());
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println("A média de avaliações deste filme é: " + meuFilme.retornaMedia());
 
     }
 }
