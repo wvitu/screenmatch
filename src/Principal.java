abfc348787139e37f2b4845c328f35fa5e24c791
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-05-13 21:46:49 -0300

    Filtros e recomendações adicionadas

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -41,5 +43,13 @@ public class Principal {
         calculadora.inclui(theBigBangTheory);
         System.out.println(calculadora.getTempoTotal());
 
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(theBigBangTheory);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme extends Titulo {
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
     private boolean emCartaz;
 
     public boolean getEmCartaz() {
@@ -9,5 +11,10 @@ public class Filme extends Titulo {
 
     public void setEmCartaz(boolean emCartaz) {
         this.emCartaz = emCartaz;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return (int) retornaMedia() / 2;
     }
 }
